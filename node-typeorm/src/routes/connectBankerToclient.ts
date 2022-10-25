16732818e2622acb9527f8b04915ec3abf26bd61
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-10-25 16:19:32 +0600

    commit pull

--- node-typeorm/src/routes/connectBankerToclient.ts
+++ node-typeorm/src/routes/connectBankerToclient.ts
@@ -6,9 +6,21 @@ const router = Router();
 
 router.put("/api/banker/:bankerId/client/:clientId", async (req, res) => {
   const { bankerId, clientId } = req.params;
-  const banker = await Banker.findOne(parseInt(bankerId));
-  const client = await Client.findOne(parseInt(clientId));
-  if (banker && client) {
-    banker.clients.push(client);
+  let banker = await Banker.findOne(parseInt(bankerId));
+  let client = await Client.findOne(parseInt(clientId));
+  if (!banker || !client) {
+    return res.status(400).json({
+      error: "Banker or client not found",
+    });
   }
+  console.log(banker.clients);
+  banker.clients = [client];
+  console.log(banker.clients);
+
+  await banker.save();
+  return res.status(200).json({
+    message: "Client added to banker",
+  });
 });
+
+export { router as connectBankerToclientRouter };
